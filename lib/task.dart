94323c896a2be5df806c52dcb0765bb5f3e63fe5
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-01-21 18:13:21 +0100

    Changed task's layout a little.

--- lib/task.dart
+++ lib/task.dart
@@ -14,10 +14,13 @@ class Task extends StatefulWidget {
 }
 
 class _TaskState extends State<Task> {
+  static const int _maxDescriptionLength = 300;
+  static const int _maxTitleLength = 30;
   final _descriptionController = TextEditingController();
   final _titleController = TextEditingController();
   String _description = '';
   String _title = '';
+  bool _expanded = true;
   Icon _trailingArrow = Icon(Icons.expand_less);
   DateTime _deadline;
   DateTime _lastModified;
@@ -52,21 +55,25 @@ class _TaskState extends State<Task> {
   Widget build(BuildContext context) {
     return Card(
       child: ExpansionTile(
-        title: Text('Title: $_title'),
-        subtitle: Text(_deadline.toString() + ' ' + widget.index.toString()),
-        // subtitle: Text('Deadline: ${_convertToIsoDate(_deadline)}'),
+        title: _renderTitle,
+        subtitle: Text('Deadline: ${_convertToIsoDate(_deadline)} ${widget.index}'),
         trailing: _trailingArrow,
         initiallyExpanded: true,
         onExpansionChanged: _changeTileExpansion,
         children: [
           Column(
             children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 32.0,
-                child: Container(
-                  child: Text(
-                    'Last modified: ${_convertToIsoDate(_lastModified)}',
-                    textAlign: TextAlign.left,
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 4.0,
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width - 40.0,
+                  child: Container(
+                    child: Text(
+                      'Last modified: ${_convertToIsoDate(_lastModified)}',
+                      textAlign: TextAlign.left,
+                    ),
                   ),
                 ),
               ),
@@ -76,26 +83,12 @@ class _TaskState extends State<Task> {
                 ),
                 child: TextField(
                   decoration: InputDecoration(
-                    labelText: 'Title',
+                    hintText: 'Description',
                     isDense: true,
-                  ),
-                  maxLength: 40,
-                  autocorrect: false,
-                  enableSuggestions: false,
-                  controller: _titleController,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16.0,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    isDense: true,
+                    counterText: '',
                   ),
                   maxLines: null,
-                  maxLength: 400,
+                  maxLength: _maxDescriptionLength,
                   autocorrect: false,
                   keyboardType: TextInputType.text,
                   enableSuggestions: false,
@@ -126,7 +119,7 @@ class _TaskState extends State<Task> {
   }
 
   void _changeTileExpansion(bool expanded) {
-    setState(() {
+   setState(() {
       if (expanded) {
         _descriptionController.text = _description;
         _titleController.text = _title;
@@ -135,6 +128,30 @@ class _TaskState extends State<Task> {
         _trailingArrow = Icon(Icons.expand_more);
       }
     });
+    _expanded = expanded;
+  }
+
+  Widget get _renderTitle {
+    if (_expanded) {
+      return Padding(
+        padding: EdgeInsets.only(
+          bottom: 8.0,
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            isDense: true,
+            hintText: 'Title',
+            counterText: '',
+          ),
+          maxLength: _maxTitleLength,
+          autocorrect: false,
+          enableSuggestions: false,
+          controller: _titleController,
+        ),
+      );
+    } else {
+      return Text('Title: $_title');
+    }
   }
 
   void _setDeadline() {
@@ -155,20 +172,18 @@ class _TaskState extends State<Task> {
   }
 
   Widget _renderRemoveButton() {
-    // Color textColor;
+    Color textColor;
 
-    // if (widget.index == 0) {
-    //   textColor = Colors.green;
-    // } else {
-    //   textColor = Colors.red;
-    // }
+    if (widget.index == 0) {
+      textColor = Colors.green;
+    } else {
+      textColor = Colors.red;
+    }
     return FlatButton(
-      // color: Theme.of(context).buttonColor,
       child: Text(
         'Remove',
         style: TextStyle(
-          // color: textColor,
-          // color: Theme.of(context).iconTheme.color,
+          color: textColor,
         ),
       ),
       onPressed: () => widget.removeTask(widget.index),
